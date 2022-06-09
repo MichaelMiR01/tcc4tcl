d53a7102c9630a0536b03ea75b2d69cec54dcf06
tcllab <97413632+tcllab@users.noreply.github.com>
2022-06-09 11:25:45 -0500

    Merge commit labeled as tcc4tcl 0.30 with version 0.30 release tarball on rkeene fossil page

--- tcc4tcl.tcl
+++ tcc4tcl.tcl
@@ -848,4 +848,4 @@ proc ::tcc4tcl::wrap {name adefs rtype {body "#"} {cname ""} {includePrototype 0
 
 namespace eval tcc4tcl {namespace export cproc}
 
-package provide tcc4tcl "@@VERS@@"
+package provide tcc4tcl "0.30"
